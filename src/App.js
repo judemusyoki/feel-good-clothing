342d6ff2381fc3ffe9227112002df98630c58190
jmusyoki <jmusyoki@wastehero.io>
2020-09-30 14:17:24 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Homepage from './pages/homepage/homepage.component';
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      {/* <Homepage /> */}
+      <Switch>  {/* Renders the first correct matched path*/}
+        <Route exact path='/' component={Homepage} /> {/* Exact renders only the homepage */}
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './directory.styles.scss';
-import MenuItem from '../menu-item/menu-item.component';
-
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
 
 class Directory extends React.Component {
   constructor() {
@@ -11,53 +10,56 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: "hats",
         },
         {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
-          linkUrl: 'shop/jackets'
+          linkUrl: "shop/jackets",
         },
         {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
-          linkUrl: 'shop/sneakers'
+          linkUrl: "shop/sneakers",
         },
         {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
           id: 4,
-          size: 'large',
-          linkUrl: 'shop/womens'
+          size: "large",
+          linkUrl: "shop/womens",
         },
         {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
           id: 5,
-          size: 'large',
-          linkUrl: 'shop/mens'
-        }
-      ]
-    }
+          size: "large",
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
   }
 
   render() {
     return (
       <div className='directory-menu'>
-        {
-          this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-          ))
-        }
+        {this.state.sections.map((
+          {
+            id,
+            ...otherSectionProps
+          } /* SectionProps simply spreads/adds the rest of the props instead of typing each twice */
+        ) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
