77fc845566fdb2054b856f32d04b0460f75d6890
jmusyoki <jmusyoki@wastehero.io>
2020-10-12 22:23:01 +0200

    adding header component and header styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
       {/* <Homepage /> */}
+      <Header />
       <Switch>  {/* Renders the first correct matched path*/}
         <Route exact path='/' component={Homepage} /> {/* Exact renders only the homepage */}
         <Route exact path='/shop' component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className='header'>
+    <Link clasName='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
